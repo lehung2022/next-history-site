aaefd47eb88b6dc39d47bafe008c39750cdce135
lehung2022 <hung.le.programmer@gmail.com>
2025-05-10 12:18:07 +0700

    I changed the links for my website

--- src/client-components/sub/source-links.ts
+++ src/client-components/sub/source-links.ts
@@ -4,18 +4,21 @@ import { DomainLinks } from "@/types/domain-links";
 export const domainLinks: DomainLinks = {
   imageSources: [
     {
-      label: "Dan toc Kinh",
-      url: "https://www.dantocking.com/p/dai-viet-ky-nhan.html",
+      label: "My Google Drive",
+      url: "https://drive.google.com/drive/folders/1ndZhok50LHEek2dbu4hajY2A_5p2XIkq?dmr=1&ec=wgc-drive-globalnav-goto",
     },
     { label: "Nobuwiki", url: "https://www.nobuwiki.org/" },
   ],
   articleSources: [
     // The article sources will not be from these two links. I am currently lazy, so I am leaving them as that. They will be changed in the future
     {
-      label: "Dan toc Kinh",
-      url: "https://www.dantocking.com/p/dai-viet-ky-nhan.html",
+      label: "Nguồn bài viết",
+      url: "https://drive.google.com/drive/folders/1ndZhok50LHEek2dbu4hajY2A_5p2XIkq?dmr=1&ec=wgc-drive-globalnav-goto",
     },
-    { label: "Nobuwiki", url: "https://www.nobuwiki.org/" },
+    {
+      label: "Lịch sử Nhật Bản và Trung Quốc",
+      url: "https://drive.google.com/drive/folders/1ndZhok50LHEek2dbu4hajY2A_5p2XIkq?dmr=1&ec=wgc-drive-globalnav-goto",
+    },
   ],
 };
 
