eacc8def0bdc3740ddad53e8524021db7f3a2243
lehung2022 <hung.le.programmer@gmail.com>
2025-05-16 12:15:46 +0700

    I will go back later

--- src/client-components/main/Footer.tsx
+++ src/client-components/main/Footer.tsx
@@ -34,7 +34,7 @@ const Footer = () => {
 
   const copyright =
     {
-      Vietnamese: `©${year} biên niên sử quả cảm. Mọi quyền được bảo lưu.`,
+      Vietnamese: `©${year} biên niên sử anh hùng. Mọi quyền được bảo lưu.`,
       Japanese: `©${year} ヴァロルのクロニクル。すべての権利を保有。`,
       Cantonese: `©${year} 勇氣編年史。保留所有權利。`,
       English: `©${year} Chronicles of Valor. All rights reserved.`,
